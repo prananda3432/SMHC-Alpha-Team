e8c35999d5416fed3a064a61c0b63e36d3c42e5f
prananda3432 <gedeprananda3107@gmail.com>
2021-05-26 18:14:58 +0800

    Add capture syntax to capstone.py

--- capstone.py
+++ capstone.py
@@ -4,19 +4,20 @@ import time
 from tensorflow.keras.models import model_from_json
 import tensorflow as tf
 import numpy as np
+import datetime as dt
 
 #start = time.time()
 faceCascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 video_capture = cv2.VideoCapture(0)
 mask_on = False
 
-class DeteksiEkspresi(object):
+class ExpressionDetection(object):
 
-    EKSPRESIWAJAH = ["Angry", "Disgust", "Fear", "Happy",
+    FACEEXPRESSION = ["Angry", "Disgust", "Fear", "Happy",
                      "Neutral", "Sad", "Shocked"]
 
     def __init__(self, file_json_model, file_weights_model):
-        # memuat model json dan weight
+        # Load model JSON and weight file
         with open(file_json_model, "r") as json_file:
             loaded_model_json = json_file.read()
             self.loaded_model = model_from_json(loaded_model_json)
@@ -24,28 +25,46 @@ class DeteksiEkspresi(object):
         
     def predict_emotion(self, img):
         self.preds = self.loaded_model.predict(img)
-        return DeteksiEkspresi.EKSPRESIWAJAH[np.argmax(self.preds)]
+        return ExpressionDetection.FACEEXPRESSION[np.argmax(self.preds)]
 
-model = DeteksiEkspresi("model.json", "weights.h5")
+model = ExpressionDetection("model.json", "weights.h5")
+t = dt.datetime.now()
+interval = 10
+danger_state = 0
+
+def ambil_ss(ssan):
+    cv2.imwrite('image-opencv.jpg', ssan)
 
 while True:
-    # ambil frame-by-frame
+    # Take frame-by-frame
     ret, frame = video_capture.read()
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     wajah = faceCascade.detectMultiScale(gray, 1.1, 5)
-
-    # gambar kotak di wajah
+    # Face box in camera
     for (x, y, w, h) in wajah:
         roi_gray = gray[y:y + h, x:x + w]
         roi_input = cv2.resize(roi_gray, (48, 48))
         
         pred = model.predict_emotion(roi_input[np.newaxis, :, :, np.newaxis])
         
+        if pred == "Angry" or "Fear" or "Sad":
+            danger_state += 1
+        
         cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 3)
         cv2.putText(frame, pred, (x, y), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 255, 0), 5)
 
     cv2.putText(frame, 'Face Detected : '+ str(len(wajah)), (30, 30), cv2.FONT_HERSHEY_SIMPLEX,  1, (0, 255, 0), 2)
     cv2.imshow('Video', frame)
+
+    current_time = dt.datetime.now()
+    delta = current_time-t
+    if delta.seconds >= 10:
+        ambil_ss(frame)
+        t = current_time.now()
+
+    if danger_state > 0:
+        ambil_ss(frame)
+        danger_state = 0
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
